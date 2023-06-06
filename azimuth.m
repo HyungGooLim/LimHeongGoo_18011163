c5f3a1ab185b77f0dfbf0d3e55c86e51b0ffc2dc
user <user@172.16.15.156>
2023-06-07 04:59:31 +0900

    230607

--- azimuth.m
+++ azimuth.m
@@ -1,15 +1,20 @@
-function  az = azimuth(ENU)
-% ENU의 위치를 이용하여 Azimuth 계산%
-% ENU성분은 [km]단위%
-ENUsize = size(ENU);
-for i=1:ENUsize(1)
-    r_E(i) = ENU(i,1); 
-    r_N(i) = ENU(i,2); 
-    r_U(i) = ENU(i,3);
+function az = azimuth(ENU)
+    % ENU의 위치를 이용하여 Azimuth 계산
+    % ENU 성분은 [km] 단위
 
-    r_EN(i) = sqrt(r_E(i)^2+r_N(i)^2);
-    az(i) = rad2deg(acos(r_N(i)./r_EN(i)));
-    if az(i)<0
-        az(i)=az(i)+360;
+    r_E = ENU(1);
+    r_N = ENU(2);
+    r_U = ENU(3);
+
+    r_EN = sqrt(r_E^2 + r_N^2);
+    az = rad2deg(acos(r_N / r_EN));
+
+    if az < 0
+        az = mod(az, 360);
     end
+
+    if r_E >= 0
+        az = az;
+    else
+        az = 360-az;
 end
--- elevation.m
+++ elevation.m
@@ -1,18 +1,16 @@
 function  el = elevation(ENU, el_mask)
 % ENU의 위치를 이용하여 Elevation 계산%
 % ENU성분은 [km]단위%
-ENUsize = size(ENU);
 mask = el_mask; %[deg]
-for i=1:ENUsize(1)
-    r_E(i) = ENU(i,1); 
-    r_N(i) = ENU(i,2); 
-    r_U(i) = ENU(i,3);
+r_E = ENU(1); 
+r_N = ENU(2); 
+r_U = ENU(3);
 
-    r_ENU(i) = sqrt(r_E(i)^2+r_N(i)^2+r_U(i)^2);
-    el(i) = asin(r_U(i)./r_ENU(i)); %[rad]
-    el(i) = rad2deg(el(i)); %[deg]
+r_ENU = sqrt(r_E^2+r_N^2+r_U^2);
+el = asin(r_U/r_ENU); %[rad]
+el = rad2deg(el); %[deg]
 
-    if el(i)<=mask
-        el(i)= NaN;
+    if el<=mask
+        el= NaN;
     end
 end
